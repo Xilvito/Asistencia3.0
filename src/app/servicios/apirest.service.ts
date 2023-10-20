910ba2461e36e68843bd78fe4a123c2674cd5d88
Diego.Cornejo <diego_ig18@hotmail.com>
2023-10-20 19:19:40 -0300

    api lista

--- src/app/servicios/apirest.service.ts
+++ src/app/servicios/apirest.service.ts
@@ -9,12 +9,17 @@ export class ApirestService {
 
   constructor(private http: HttpClient) { }
 
-  urlApi = "https://pokeapi.co/api/v2/" 
-  token = "100"
+  urlApi = "https://one-piece-episodes.p.rapidapi.com/one_piece/seasons" 
 
   obtenerAsesores(): Observable<any>
   {
-    return this.http.get(this.urlApi+"obtenerAsesores/"+this.token).pipe();
+    return this.http.get('https://one-piece-episodes.p.rapidapi.com/one_piece/seasons', {
+      headers: {
+      'X-RapidAPI-Key': '95d6dadc0amshfbfd14d50c65499p169205jsnd1b7d7d1ef0e',
+      'X-RapidAPI-Host': 'one-piece-episodes.p.rapidapi.com'
+    },
+    params: {language: 'es'}
+    });
   }
 
 
